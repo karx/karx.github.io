3d45e7de5c305b9ca47e67703c95858b1fe9332d
Kartik <kartik.arora1214@gmail.com>
2019-06-08 01:51:13 +0530

    tick logic

--- Socks/demo/main.js
+++ Socks/demo/main.js
@@ -23,20 +23,28 @@ AFRAME.registerComponent('marker-logger', {
 
 	},
 	tick: function (time, timeDelta) {
+		
 		var directionVec3 = this.directionVec3;
 		var currentPosition = this.el.object3D.position;
-		console.log(prevPosition);
-		console.log(currentPosition);
-		directionVec3.copy(prevPosition).sub(currentPosition);
-		var distance = directionVec3.length();
-		if (distance < 1) { return; }
-		var factor = this.data.speed / distance;
-		['x', 'y', 'z'].forEach(function (axis) {
-		  directionVec3[axis] *= factor * (timeDelta / 1000);
-		});
-		console.log("Moved!! " + this.el.id);
-		console.log(directionVec3);
-
+		if (this.prevPosition)
+		{
+			console.log(currentPosition);
+			directionVec3.copy(prevPosition).sub(currentPosition);
+			var distance = directionVec3.length();
+			if (distance < 1) { return; }
+			var factor = this.data.speed / distance;
+			['x', 'y', 'z'].forEach(function (axis) {
+			  directionVec3[axis] *= factor * (timeDelta / 1000);
+			});
+			console.log("Moved!! " + this.el.id);
+			console.log(directionVec3);
+			this.prevPosition = currentPosition;
+	
+		} else {
+			this.prevPosition = currentPosition;
+		}
+		// console.log(prevPosition);
+		
 	}
 
 	});
