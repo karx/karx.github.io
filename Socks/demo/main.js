58dcf93fbc8b7cde4b74cd4e129f1d07e41a7a96
Kartik <kartik.arora1214@gmail.com>
2019-06-08 02:23:25 +0530

    new logger with tock

--- Socks/demo/main.js
+++ Socks/demo/main.js
@@ -16,35 +16,21 @@ AFRAME.registerComponent('registerevents', {
 
 
 AFRAME.registerComponent('marker-logger', {
+	multiple: true,
+
 	init: function () {
 		console.log('Hello, LoggerInit');
 		this.directionVec3 = new THREE.Vector3();
 		this.prevPosition = this.el.object3D.position;
 
 	},
-	tick: function (time, timeDelta) {
+	tock: function (time, timeDelta) {
 		
-		var directionVec3 = this.directionVec3;
 		var currentPosition = this.el.object3D.position;
-		if (this.prevPosition)
-		{
-			console.log(currentPosition);
-			directionVec3.copy(this.prevPosition).sub(currentPosition);
-			var distance = directionVec3.length();
-			if (distance < 1) { return; }
-			var factor = this.data.speed / distance;
-			['x', 'y', 'z'].forEach(function (axis) {
-			  directionVec3[axis] *= factor * (timeDelta / 1000);
-			});
-			console.log("Moved!! " + this.el.id);
-			console.log(directionVec3);
-			this.prevPosition = currentPosition;
-	
-		} else {
-			this.prevPosition = currentPosition;
-		}
-		// console.log(prevPosition);
+		console.log(currentPosition);
+		console.log(this.el.id);
 		
 	}
+	
 
 	});
