6005e00615ff91370b905b2478d19c4d9448cf50
kaaro-UbuntuPC <kartik.arora1214@gmail.com>
2019-08-06 21:34:23 +0530

    fix bugs

--- face-demo/rahul.js
+++ face-demo/rahul.js
@@ -1,63 +1,67 @@
-  var firestore = firebase.firestore();
-  console.log('starting');
+var firestore = firebase.firestore();
+console.log('starting');
 
-  var face_id_list = [];
+var face_id_list = [];
+var last_face_guy_unsub = null;
 
-  firestore.collection("users-face")
-    .onSnapshot(function(querySnapshot) {
+firestore.collection("users-face")
+  .onSnapshot(function (querySnapshot) {
+    console.log(querySnapshot);
+    var faces = [];
+    querySnapshot.forEach(function (doc) {
+      faces.push({
+        face_id: doc.data().FaceId,
+        image_byte: doc.data().ImageByte,
+        timestamp: doc.data().Timestamp
+      });
+    });
+    console.log(faces.length);
+    var html_to_push = "";
+
+    faces.forEach((contestant) => {
+      var cst = contestant;
+      console.log(cst);
+      if (!cst.face_id) {
+        ;
+      } else {
+        html_to_push += `<div onclick="get_face_instances('${cst.face_id}')" class="each_face"> <div class="id">${cst.face_id}</div> <img id="profileImage" src="data:image/jpg;base64, ${cst.image_byte.toBase64()}"> </div>`;
+      }
+    });
+    $('#registered').html(html_to_push);
+  });
+
+
+function get_face_instances(face_id) {
+  if (last_face_guy_unsub) {
+    last_face_guy_unsub();
+  }
+  last_face_guy_unsub = firestore.collection(`face-instances`).where("FaceId", "==", face_id)
+    .onSnapshot(function (querySnapshot) {
       console.log(querySnapshot);
-        var faces = [];
-        querySnapshot.forEach(function(doc) {
-            faces.push( {
-                face_id: doc.data().FaceId,
-                image_byte: doc.data().ImageByte,
-                timestamp: doc.data().Timestamp
-            });
+      var instances = [];
+      querySnapshot.forEach(function (doc) {
+        instances.push({
+          face_id: doc.data().FaceId,
+          image_byte: doc.data().ImageByte,
+          timestamp: doc.data().Timestamp
         });
-        console.log(faces.length);
-        var html_to_push = "";
-        
-        faces.forEach((contestant) => {
-            var cst = contestant;
-            console.log(cst);
-            if (!cst.face_id) {
-                ;
-            } else {
-                html_to_push += `<div onclick="get_face_instances('${cst.face_id}')" class="each_face"> ${cst.face_id} <img id="profileImage" src="data:image/jpg;base64, ${cst.image_byte.toBase64()}"> </div>`;
-            }
-        });
-        $('#registered').html(html_to_push);
+      });
+      console.log(instances.length);
+      var html_to_push = "";
+      instances.forEach((contestant) => {
+        var cst = contestant;
+        console.log(cst);
+        if (!cst.face_id) {
+          ;
+        } else {entries
+          html_to_push += `<div class="each_face each_instance"> <div class="time">${cst.timestamp}</div> <img id="profileImage" src="data:image/jpg;base64, ${cst.image_byte.toBase64()}"> </li>`;
+        }
+      });
+      $('#entries').html(html_to_push);
     });
 
+}
 
-    function get_face_instances(face_id) {
-      firestore.collection(`mange-users-face/${face_id}/attendance`)
-      .onSnapshot(function(querySnapshot) {
-        console.log(querySnapshot);
-          var instances = [];
-          querySnapshot.forEach(function(doc) {
-              instances.push( {
-                  face_id: doc.data().FaceId,
-                  image_byte: doc.data().ImageByte,
-                  timestamp: doc.data().Timestamp
-              });
-          });
-          console.log(instances.length);
-          var html_to_push = "";
-          instances.forEach((contestant) => {
-              var cst = contestant;
-              console.log(cst);
-              if (!cst.face_id) {
-                  ;
-              } else {
-                  html_to_push += `<li> ${new Date(cst.timestamp)} <img id="profileImage" src="data:image/jpg;base64, ${cst.image_byte.toBase64()}"> </li>`;
-              }
-          });
-          $('#entries').html(html_to_push);
-      });
-    
-    }
-
-$(".each_face").click( (e) => {
+$(".each_face").click((e) => {
   console.log(e);
 })
