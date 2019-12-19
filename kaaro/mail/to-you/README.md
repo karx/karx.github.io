0995603fef1044793471081ccecb26bbc2836b2c
Kartik <kaaro@Apples-MacBook-Pro.local>
2019-12-19 18:12:48 +0530

    updates and mqtt js

--- kaaro/mail/to-you/README.md
+++ kaaro/mail/to-you/README.md
@@ -20,7 +20,7 @@ Social Media + On ground feedback
 ### Top activites
 * Wendor Events at DLF mall of India : [dlf-moi](https://akriya.co.in/wendor-events/)
 * Updated homepage of our partners: [Viragram Media](https://viragram.me/)
-* First lts-version of Smart Home solution: [Link](https://github.com/ashtam55/smart_switch/releases)
+* First LTS-version of Smart Home solution: [Link](https://github.com/ashtam55/smart_switch/releases)
 * Abacus Smart - our Smart Home solution App live on Play Store: [Play store Link](https://play.google.com/store/apps/details?id=com.artiosys.smart_switch)
 * ESP32 talk with ILUG-delhi and Delhi HHC: [Link](https://github.com/hardware-hackers-club-delhi/Talks/issues/3)
 * BLE logger for our connected devices: [Link](https://github.com/ashtam55/BleWebLogger)
@@ -31,3 +31,6 @@ Social Media + On ground feedback
 * ESP32 Object Detection for Malboro using IR : [Link](https://github.com/ashtam55/Digital_Icon/tree/malboro/blstr/recvr)
 * ESP32 Weight Load cell for small weights : [Link]()
 * BLE Mesh modules for all our connected devices.
+
+### Webcomponents 
+* TBAdded
--- mqtt/index.js
+++ mqtt/index.js
@@ -5,9 +5,10 @@ var client = mqtt.connect('http://api.akriya.co.in');
 
 client.on('connect', function () {
     
-    client.publish('homeSwitch/ready/kaaroo','iamon');
-    client.publish('homeSwitch/ready/2761046813','Ready!');
+    // client.publish('HS/303385001/status','11110000');
+    // client.publish('homeSwitch/ready/2761046813','sReady!');
     console.log("published");
+    start();
     client.subscribe('HS/#', function (err) {
         if (!err) {
             client.publish('HS/presence', 'Connected')
@@ -22,6 +23,9 @@ client.on('connect', function () {
             console.error(err);
         }
     });
+    client.subscribe('Kento/#');
+    client.subscribe('digitalicon/#');
+    
 });
 
 
@@ -29,3 +33,35 @@ client.on('message', function (topic, message) {
     // message is Buffer
     console.log(`${topic}: ${message.toString()}`);
 });
+
+
+function turnOn() {
+    let statusString = '';
+    for (let i=0;i<8;i++) {
+        let bit = Math.floor((Math.random() * 1000000) + 1)%2; 
+        statusString += bit;    
+    }
+    
+    client.publish('HS/303385001/status',statusString);
+}
+
+function turnOff() {
+    client.publish('HS/303385001/status','00000000');
+}
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function sleep(fn, ...args) {
+    await timeout(3000);
+    return fn(...args);
+}
+async function start() {
+    
+    for(let i=0;i<100;i++) {
+        console.log(i);
+        turnOn();
+        await timeout(1750);
+    }
+}
+
+
